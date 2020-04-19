b8162a427d314fa78863fd3a8b0e765eb136a8c7
Unnikrishnan Ambalapatt <49778290+unnikrishnan-ambalapatt@users.noreply.github.com>
2020-04-18 20:01:37 -0700

    Two Sum problem

---
+++ src/TwoSum.java
@@ -0,0 +1,31 @@
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * <p>
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * Example:
+ * <p>
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * <p>
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+
+public class TwoSum {
+
+    static int[] sumOfTwo(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] ints = sumOfTwo(new int[]{2, 7, 11, 15}, 9);
+        System.out.println(ints[0] + ", " + ints[1]);
+    }
+}
