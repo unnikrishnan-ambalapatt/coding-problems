d8248e6f9f1cc836a19dae8a57476846e5be5ec0
Unnikrishnan Ambalapatt <49778290+unnikrishnan-ambalapatt@users.noreply.github.com>
2020-04-21 17:49:15 -0700

    Added question details

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -2,6 +2,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 1. Two Sum
+ *
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * <p>
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
