d770c8aa4d59b12aeec5dba0786d7a2ad4ce7139
Unnikrishnan Ambalapatt <49778290+unnikrishnan-ambalapatt@users.noreply.github.com>
2020-04-25 16:56:43 -0700

    ZigZag conversion - First step completed

--- src/ZigZagConversion.java
+++ src/ZigZagConversion.java
@@ -38,6 +38,51 @@ public class ZigZagConversion {
         //Go top to botton until 'numRows' is reached.
         //Then go diagonally from bottom-left to top-right 'numRows' times.
         //Repeat both the above steps until all characters are run out from s.
+
+        while (true) {
+            if (charPointer >= s.length()) {
+                break;
+            }
+            while (true) {
+                if (null == arr[rowCounter]) {
+                    arr[rowCounter] = new String[20];
+                }
+                arr[rowCounter][colCounter] = " ";
+                arr[rowCounter][colCounter] = s.substring(charPointer, charPointer + 1);
+                charPointer++;
+                if (charPointer >= s.length()) {
+                    break;
+                }
+                rowCounter++;
+                if (rowCounter >= numRows) {
+                    break;
+                }
+            }
+            rowCounter--;
+            charPointer--;
+            while (true) {
+                rowCounter--;
+                if (rowCounter == 0) {
+                    break;
+                }
+                colCounter++;
+                charPointer++;
+                if (charPointer >= s.length()) {
+                    break;
+                }
+                arr[rowCounter][colCounter] = s.substring(charPointer, charPointer + 1);
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] == null ? " " : arr[i][j] + " ");
+            }
+            System.out.println();
+        }
         return convertedStr;
+    }
+
+    public static void main(String[] args) {
+        convertToZigZag("PAYPALISHIRING", 4);
     }
 }
