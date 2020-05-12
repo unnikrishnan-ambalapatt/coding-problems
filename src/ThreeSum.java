a02f904268a7549d60ba5e79f4c8dc945ce85cfd
Unnikrishnan Ambalapatt <49778290+unnikrishnan-ambalapatt@users.noreply.github.com>
2020-05-12 16:31:06 -0700

    Three Sum - Init version

---
+++ src/ThreeSum.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 15. 3Sum
+ * <p>
+ * Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+ * <p>
+ * Note:
+ * <p>
+ * The solution set must not contain duplicate triplets.
+ * <p>
+ * Example:
+ * <p>
+ * Given array nums = [-1, 0, 1, 2, -1, -4],
+ * <p>
+ * A solution set is:
+ * [
+ * [-1, 0, 1],
+ * [-1, -1, 2]
+ * ]
+ */
+public class ThreeSum {
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> tripletList = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (j == i) {
+                    continue;
+                }
+                for (int k = 0; k < nums.length; k++) {
+                    if (k == i || k == j) {
+                        continue;
+                    }
+                    if (0 == (nums[i] + nums[j] + nums[k])) {
+                        List<Integer> triplets = new ArrayList<>();
+                        triplets.add(nums[i]);
+                        triplets.add(nums[j]);
+                        triplets.add(nums[k]);
+                        tripletList.add(triplets);
+                    }
+                }
+            }
+        }
+        return tripletList;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(arr));
+        int[] arr2 = {-1, 0, 1, 2, -1, -4, 5, 2};
+        System.out.println(threeSum(arr2));
+        int[] arr3 = {3, 4, -2};
+        System.out.println(threeSum(arr3));
+        int[] arr4 = {7, 3, 2};
+        System.out.println(threeSum(arr4));
+    }
+}
