a389c5bd22e1f83d2aeba1b3270b7fe17057efcb
Unnikrishnan Ambalapatt <49778290+unnikrishnan-ambalapatt@users.noreply.github.com>
2020-05-11 18:46:02 -0700

    Longest common prefix - Basic solution

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 14. Longest Common Prefix
  * <p>
@@ -20,20 +23,23 @@
  */
 public class LongestCommonPrefix {
 
-    public String longestCommonPrefix(String[] strs) {
-        String longest = "", temp = "";
-        int position = 1;
-        temp = strs[0].substring(0, position);
-        for (int i = 1; i < strs.length; i++) {
-            if(!temp.equals(strs[i].substring(0, position))) {
-
+    public static String longestCommonPrefix(String[] strs) {
+        String longest = "";
+        Set<String> chars = new HashSet<>();
+        int position = 0;
+        do {
+            for (int i = 0; i < strs.length; i++) {
+                chars.add(strs[i].substring(position, position + 1));
             }
-            temp = strs[i].substring(0, position);
-        }
+            longest = strs[0].substring(0, position + 1);
+            position++;
+        } while (chars.size() == 1);
+
         return longest;
     }
 
     public static void main(String[] args) {
-
+        String[] arr = {"flower", "flow", "flight"};
+        System.out.println(longestCommonPrefix(arr));
     }
 }
